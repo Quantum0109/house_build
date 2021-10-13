5c6a8555d3f0499117bc01f2469a366a08526197
Quantum0109 <72807872+Quantum0109@users.noreply.github.com>
2021-10-13 17:40:27 +0300

    Add files via upload

---
+++ house_building.py
@@ -0,0 +1,25 @@
+def build_house(base_x = -500, base_y = -500):
+	"""
+		base_x - x левого нижнего угла фундамента
+		base_y - y левого нижнего угла фундамента
+	"""
+	print(f"Будем строить дом в {base_x} и {base_y}")
+
+	def build_base():
+		print("Приехала бригада битонщиков")
+	
+	
+	def build_walls():
+		print("Приехала бригада стен")
+	
+
+	def build_roof():
+		print("Приехала бригада крыш")
+
+
+
+	build_base()
+	build_walls()
+	build_roof()
+
+build_house(base_x = 0, base_y = 0)
