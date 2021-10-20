e6f529603d553c24f9974e1a3b32a7c49036e7e8
Quantum0109 <72807872+Quantum0109@users.noreply.github.com>
2021-10-20 17:36:54 +0300

    Add files via upload

--- house_building.py
+++ house_building.py
@@ -1,6 +1,5 @@
 import turtle as t
 
-
 def build_house(base_x = -500, base_y = -500, base_width = 100, base_height = 10, base_fill = "#990000", walls_width = 0, walls_height = 0, walls_fill = "#000000", roof_width = 0, roof_height = 0, roof_fill = "#000000"):
     """
         base_x - x левого нижнего угла фундамента
@@ -17,48 +16,71 @@ def build_house(base_x = -500, base_y = -500, base_width = 100, base_height = 10
         walls_fill - спрашиваем у заказчика
 
         roof_x - считаем автоматически
-        roof_y - - считаем автоматически
-        roof_width - спрашиваем у заказчика
+        roof_y - считаем автоматически
+        roof_width - считаем автоматически, 120% ширины крыши
         roof_height - спрашиваем у заказчика
         roof_fill - спрашиваем у заказчика
     """
-    t.speed(1)
+    t.speed(0)
 
-    def draw_rectangle(x, y, width = 0, height = 0, fill = "#000000"):
+    #считаем автоматически величины
+    walls_x = base_x
+    walls_y = base_y + base_height
+    walls_width = base_width
+    roof_x = walls_x - (walls_width * 0.1)
+    roof_y = base_height + walls_height
+    roof_width = walls_width * 1.2
+
+    def build_base(base_x, base_y, base_width, base_height, base_fill):
         t.penup()
-        t.goto(x, y)
+        t.goto(base_x, base_y)
+        t.setheading(0)
+        t.fillcolor(base_fill)
+        t.pendown()
+        t.begin_fill()
+        t.forward(base_width)
+        t.left(90)
+        t.forward(base_height)
+        t.left(90)
+        t.forward(base_width)
+        t.left(90)
+        t.forward(base_height)
+        t.end_fill()
+
+    def build_walls(walls_x, walls_y, walls_width, walls_height, walls_fill):
+        t.penup()
+        t.goto(walls_x, walls_y)
         t.setheading(0)
         t.pendown()
-        t.fillcolor(fill)
+        t.fillcolor(walls_fill)
         t.begin_fill()
-        t.forward(width)
+        t.forward(walls_width)
         t.left(90)
-        t.forward(height)
+        t.forward(walls_height)
         t.left(90)
-        t.forward(width)
+        t.forward(walls_width)
         t.left(90)
-        t.forward(height)
+        t.forward(walls_height)
         t.left(90)
         t.end_fill()
 
 
-    def build_base(base_x, base_y, base_width, base_height, base_fill):
-        draw_rectangle(base_x, base_y,base_width,base_height, base_fill)
-    
-    def build_walls(walls_width, walls_height, walls_fill):
-        walls_x = 10
-        walls_y = 10
-        draw_rectangle(walls_x, walls_y, walls_width, walls_height, walls_fill)
-    
-
-    def build_roof():
-        print("Приехала бригада крыш")
-
+    def build_roof(roof_x, roof_y, roof_width, roof_height, roof_fill):
+        t.penup()
+        t.goto(roof_x, roof_y)
+        t.setheading(0)
+        t.pendown()
+        t.fillcolor(roof_fill)
+        t.begin_fill()
+        t.forward(roof_width)
+        t.goto(walls_width / 2, base_height + walls_height + roof_height)
+        t.goto(roof_x, roof_y)
+        t.end_fill()
 
 
     build_base(base_x, base_y, base_width, base_height, base_fill)
-    build_walls(walls_width, walls_height, walls_fill)
-    build_roof()
+    build_walls(walls_x, walls_y, walls_width, walls_height, walls_fill)
+    build_roof(roof_x, roof_y, roof_width, roof_height, roof_fill)
 
-build_house(base_x = 0, base_y = 0, base_width = 200, base_height = 10, base_fill = "#000066")
+build_house(base_x = 0, base_y = 0, base_width = 200, base_height = 10, base_fill = "#660000", walls_height = 150, walls_fill = "#00FF00", roof_height = 100, roof_fill = "#FFFF00")
 t.done()
